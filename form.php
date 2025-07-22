29b1cce827362ff9634f8e50749a0029b52428ea
Deepika <palakondadeepika25@gmail.com>
2025-07-22 12:05:45 +0530

    deploy

--- form.php
+++ form.php
@@ -5,14 +5,15 @@
 </head>
 <body>
 <?php
-
-$dbc = mysqli_connect('localhost','root','','detective')
+$servername="deepuprojects.rf.gd";
+$username="if0_39530181";
+$password="Glw68tJLNpGS";
+$dbc = mysqli_connect($servername,$username,$password,'detective')
 or die('error in establishing connection to database');
 $query = "insert into person(first_name,last_name,email,enquiry,description,kidnappers_count,comments)values('".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['ask']."','".$_POST['description']."','".$_POST['aliens']."','".$_POST['comments']."')";
 $result =mysqli_query($dbc,$query);
 if(!$result){
     die('error in executing query'.mysqli_error($dbc));
-
 }
 else
 {echo "<p> inserted successfully</p>";}
